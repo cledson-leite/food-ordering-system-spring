b47476d0bb0c389ee605e24770664541dd3a9c25
Cledson Leite <csbetsonline@gmail.com>
2023-11-01 21:23:25 -0300

    refactor in method create event

--- order-service/src/main/java/com/cledsonleite/orderservice/service/OrderCreateCommandHandler.java
+++ order-service/src/main/java/com/cledsonleite/orderservice/service/OrderCreateCommandHandler.java
@@ -9,6 +9,7 @@ import com.cledsonleite.orderservice.core.useCase.IDomainService;
 import com.cledsonleite.orderservice.service.dto.create.CreateOrderCommand;
 import com.cledsonleite.orderservice.service.dto.create.CreateOrderResponse;
 import com.cledsonleite.orderservice.service.mapper.OrderDataMapper;
+import com.cledsonleite.orderservice.service.publisher.IOrderCreatePaymentRequestMessagePublisher;
 import com.cledsonleite.orderservice.service.repository.ICustomerRepository;
 import com.cledsonleite.orderservice.service.repository.IOrderRepository;
 import com.cledsonleite.orderservice.service.repository.IRestaurantRepository;
@@ -23,66 +24,20 @@ import java.util.UUID;
 @Slf4j
 @Component
 public class OrderCreateCommandHandler {
-    @Autowired
-    private IDomainService useCase;
 
     @Autowired
-    private IOrderRepository orderRepository;
+    private IOrderCreatePaymentRequestMessagePublisher publisher;
 
     @Autowired
-    private ICustomerRepository customerRepository;
-
-    @Autowired
-    private IRestaurantRepository restaurantRepository;
+    private OrderCreateHelper helper;
 
     @Autowired
     private OrderDataMapper mapper;
 
-    @Transactional
+
     public CreateOrderResponse createOrder(CreateOrderCommand command) {
-        checkCustomer(command.getCustomerId());
-        Restaurant restaurant = checkRestaurant(command);
-        Order order = mapper.toOrder(command);
-        OrderCreateEvent event = useCase.validateAndInitiateOrder(order, restaurant);
-        Order orderResponse = saveOrder(order);
-        log.info("Pedido com id { } criado com sucesso!", orderResponse.getId().getId());
-        return mapper.toCreateOrderResponse(orderResponse);
-    }
-
-    private Order saveOrder(Order order) {
-        Order orderResponse = orderRepository.save(order);
-        if (orderResponse == null){
-            log.error("Pedido não pode ser salvo!");
-            throw  new OrderDomainException("Pedido não pode ser salvo!");
-        }
-        log.info("Pedido salvo com id { }", orderResponse.getId().getId());
-        return orderResponse;
-    }
-
-    private Restaurant checkRestaurant(CreateOrderCommand command) {
-        Restaurant restaurant = mapper.toRestaurant(command);
-        Optional<Restaurant> optionalRestaurant = restaurantRepository
-                .findRestaurantInformation(restaurant);
-        if (optionalRestaurant.isEmpty()) {
-            log.warn("Restaurante com id { } não encontrado", restaurant.getId().getId());
-            throw new OrderDomainException(
-                    String.format(
-                            "Restaurante com id { } não encontrado", restaurant.getId().getId()
-                    )
-            );
-        }
-        return optionalRestaurant.get();
-    }
-
-    private void checkCustomer(UUID customerId) {
-        Optional<Customer> customer = customerRepository.findCustomer(customerId);
-        if (customer.isEmpty()) {
-            log.warn("Cliente com id { } não encontrado", customerId);
-            throw new OrderDomainException(
-                    String.format(
-                            "Cliente com id { } não encontrado", customerId
-                    )
-            );
-        }
+        OrderCreateEvent event = helper.persistOrder(command);
+        publisher.publish(event);
+        return mapper.toCreateOrderResponse(event.getOrder());
     }
 }
